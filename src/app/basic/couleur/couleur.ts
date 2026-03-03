cebcf7b0555f01a4d635a4e5646c3fb93a99af67
vgaudel <vgaudel@gmail.com>
2026-03-03 15:41:11 +0100

    tableau carrés avec couleur

--- src/app/basic/couleur/couleur.ts
+++ src/app/basic/couleur/couleur.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-couleur',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './couleur.html',
   styleUrl: './couleur.css',
 })
-export class Couleur {}
+export class Couleur {
+
+  listeCouleurs = ["blue","red","black"];
+  couleur : string = "black";
+  /* <select>  
+      <option> blue</option>
+      <option> red</option>
+      <option> black</option>
+    </select>*/
+  message : string = "Quelques valeurs au carré";
+  valeurs : number[] = [1,2,4,8,16];
+  nouvelleValeur: number = 0;
+
+  onAjoutNouvelleValeur(){
+    this.valeurs.push(this.nouvelleValeur);
+  }
+}
